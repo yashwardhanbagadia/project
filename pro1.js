0486c5e2910bc46e2080b42cdf5ae28ed45f3026
yashwardhanbagadia <166416543+yashwardhanbagadia@users.noreply.github.com>
2024-06-02 12:49:22 +0530

    pro1.js

--- pro1.js
+++ pro1.js
@@ -2,38 +2,42 @@ function handleFormSubmit(event)
 {
   event.preventDefault();
 let myObj={
-   email: event.target.email.value,
-   phone : event.target.phone.value,
-     username: event.target.username.value
+   exp: event.target.exp.value,
+   des : event.target.des.value,
+   cat: event.target.cat.value
 
  }
 let m=JSON.stringify(myObj)
-localStorage.setItem("User Details", m)
+localStorage.setItem(myObj.exp, m)
 const li1=document.createElement('li');
- 
-
-  
-  const t1=document.createTextNode(event.target.username.value+" "+event.target.email.value+" "+event.target.phone.value)
-  li1.appendChild(t1);
-   
-
+const t1=document.createTextNode(event.target.exp.value+" "+event.target.des.value+" "+event.target.cat.value)
+li1.appendChild(t1);
 const u=document.querySelector('ul');
-  u.appendChild(li1)
-   
+u.appendChild(li1)
 const b=document.createElement('button')
   const bt=document.createTextNode('Delete')
   b.appendChild(bt)
   b.className='del'
   li1.appendChild(b)
+  const editBtn = document.createElement("button");
+    editBtn.appendChild(document.createTextNode("Edit"));
+    li1.appendChild(editBtn);
   u.addEventListener('click',function(event){
   if(event.target.classList.contains('del'))
     {
-      const fruitsToDelete=event.target.parentElement;
-      console.log(fruitsToDelete)
-      u.removeChild(fruitsToDelete)
+     
+      u.removeChild(event.target.parentElement)
       localStorage.removeItem("User Details")
 
     }
 })
+editBtn.addEventListener("click", function (event) {
+  u.removeChild(event.target.parentElement);
+  localStorage.removeItem(myObj.exp)
+
+  document.getElementById("exp").value = myObj.exp;
+  document.getElementById("des").value = myObj.des;
+  document.getElementById("cat").value = myObj.cat;
+});
 }
 
